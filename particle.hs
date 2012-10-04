ad9f87367d6b18faa88cce1c571cc94a03c799aa
Nebuta <fullerenec84@gmail.com>
2012-10-04 11:36:04 -0700

    particle detection threshold added

--- particle.hs
+++ particle.hs
@@ -4,16 +4,20 @@ import CV.Core
 import CV.Filter
 import CV.Demo
 
+import Data.List (sort)
+
 findParticles :: GrayImage -> [Pos]
 findParticles = detect . prefilter
   where prefilter = gauss 3
 
 detect :: GrayImage -> [Pos]
-detect img = filterThresAndDist percentileThres distThres $ refinePos $ findIndexMat cmpEqual img dilated
+detect img = refinePos $ filterThresAndDist minInt minDist img $ findIndexMat cmpEqual img dilated
   where
-    percentileThres = 10    -- in %
-    distThres = 4
+    percentile = 10.0   -- in %
+    minInt = fromIntegral $ intensities !! (floor $ percentile / 100.0 * fromIntegral (length intensities))
+    minDist = 4
     dilated = dilate (fromStrEl (Ellipse 3 3)) img
+    intensities = (reverse . sort . concat . pixels) img
 
 findIndexMat :: CmpFun a b c -> MatT a b c -> MatT a b c-> [Coord]
 findIndexMat f a b = findNonZero $ CV.Core.compare f a b
@@ -22,11 +26,11 @@ refinePos :: [Coord] -> [Pos]
 refinePos cs = map f cs
   where f (Coord y x) = Pos (fromIntegral y) (fromIntegral x)--Stub
 
-filterThresAndDist :: Double -> Double -> [Pos] -> [Pos]
-filterThresAndDist thres dist ps = filter (f thres dist ps) ps
+filterThresAndDist :: Double -> Double -> MatT U8 C1 Gray -> [Coord] -> [Coord]
+filterThresAndDist int dist mat ps = filter (f int dist mat ps) ps
   where
-    f :: Double -> Double -> [Pos] -> Pos -> Bool
-    f th di ps p = True -- Stub
+    f :: Double -> Double -> MatT U8 C1 Gray -> [Coord] -> Coord -> Bool
+    f int dist mat ps (Coord y x) = (fromIntegral $ pixelAt y x mat) >= int  -- Stub: add dist filter
 
 data Traj = Traj [Pos3D]
 
@@ -42,7 +46,7 @@ addFrameIdx pss = zipWith f [0..length pss-1] pss
     f i ps = map (g i) ps
     g i (Pos x y) = Pos3D i x y
 
-main = demos
+main = maintrue
 
 maintrue :: IO ()
 maintrue = do
